9cf07dc4e3ff23026242921b2f3ad33d33402a9f
sergiupanaitescu <sergiu.panaitescu@radcom.com>
2021-09-08 19:02:32 +0300

    Add user DTO class

---
+++ src/main/java/com/sergiu/authenticationdemo/dto/UserDTO.java
@@ -0,0 +1,60 @@
+package com.sergiu.authenticationdemo.dto;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+
+@JsonInclude(Include.NON_NULL)
+public class UserDTO {
+
+	private String username;
+	
+	private String name;
+	
+	private int age;
+	
+	private String address;
+	
+	private String token;
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getToken() {
+		return token;
+	}
+
+	public void setToken(String token) {
+		this.token = token;
+	}
+	
+	
+}
