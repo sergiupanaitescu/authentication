21bc52122b4d2ab3584f9f6e57972142de3a6652
sergiupanaitescu <sergiu.panaitescu@radcom.com>
2021-09-08 18:18:24 +0300

    Base commit generated with spring initializer

---
+++ src/main/java/com/sergiu/authenticationdemo/AuthenticationDemoApplication.java
@@ -0,0 +1,13 @@
+package com.sergiu.authenticationdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthenticationDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthenticationDemoApplication.class, args);
+	}
+
+}
